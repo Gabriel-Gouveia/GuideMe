bdee00dab3bfc1a61958e0461bd6a0cdda2903be
Gabriel-Gouveia <gabriel_gouveia5@hotmail.com>
2023-04-30 21:44:30 -0300

    [FEAT] Obtenção de permissões necessárias para usar o Bluetooth
    Esse commit possibilita obter as permissões para usar o Bluetooth do celular.
    
    As permissões necessárias são: ACCESS_COARSE_LOCATION e ACCESS_FINE_LOCATION.
    
    Essas permissões são necessárias para permitir o escaneamento de dispositivos Bluetooth e a conexão Bluetooth.

--- GuideMe/GuideMe/MainPage.xaml.cs
+++ GuideMe/GuideMe/MainPage.xaml.cs
@@ -5,14 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using GuideMe.Services;
+using System.Diagnostics;
 
 namespace GuideMe
 {
     public partial class MainPage : ContentPage
     {
+        public PermissionStatus PermissaoBLE { get; set; } = PermissionStatus.Unknown;
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BluetoothService bluetoothService = new BluetoothService();
+            PermissaoBLE = await bluetoothService.ObtemPermissao();
+
+            // Perform other tasks after the permission is granted
         }
     }
 }
--- GuideMe/GuideMe/Services/BluetoothService.cs
+++ GuideMe/GuideMe/Services/BluetoothService.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace GuideMe.Services
 {
-    class BluetoothService
+    public class BluetoothService
     {
+        /// <summary>
+        /// Obtém as permissões necessárias para escanear e conectar com dispositivos Bluetooths.
+        /// As duas permissões necessárias são: ACCESS_COARSE_LOCATION e ACCESS_FINE_LOCATION.
+        /// Esse método já busca conseguir essas permissões durante o uso do app.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<PermissionStatus> ObtemPermissao()
+        {
+            PermissionStatus permissionStatus = PermissionStatus.Unknown;
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                permissionStatus = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+                if (permissionStatus != PermissionStatus.Granted)
+                {
+                    permissionStatus = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                    if (permissionStatus != PermissionStatus.Granted)
+                    {
+                        permissionStatus = PermissionStatus.Denied; // Permission denied
+                    }
+                    else
+                    {
+                        permissionStatus = PermissionStatus.Granted;
+                    }
+                }
+            }
+            return permissionStatus;
+        }
     }
 }
